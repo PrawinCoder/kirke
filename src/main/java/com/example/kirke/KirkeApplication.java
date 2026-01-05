448d67b7d4bc73d85e113d18b32259084a048431
PraveenK50i <praveensunki1234@gmail.com>
2026-01-05 08:33:31 +0530

    swagger json added

---
+++ src/main/java/com/example/kirke/KirkeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kirke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KirkeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kirke/KirkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kirke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
